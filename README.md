3de8e945dd380efaac5841d3603c74a4c9b741dd
hanspbm <hanspbm@gmail.com>
2016-09-17 15:54:20 -0400

    Some file changes after not touching the site for a year.

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 The purpose of this site is to provide Puzzle and Dragons players convenience by providing them tools to help them with their adventure. 
 
 Any constructive critism with the code or how the site is set up is welcome! I hope you guys enjoy. =]
+
+--------------------------------
+
+Future Project Implementations:
+- Keep track of user's rank
+	- to get an idea of how much stamina they have
+- Send info to user about current day's hourly dungeons
+	- Via text, email or smart watch
+
+--------------------------------
+
+Install These Developer Tools:
+- Browsersync
--- index.php
+++ index.php
@@ -8,60 +8,83 @@
 	<script src="jquery-1.11.2.js"></script>
 
     <script>
-		$( document ).ready(function() {
-		    $( ".user-stamina" ).keyup(function() {
+		$(document).ready(function() {
 
-		    	var stamina = $( this ).val();
+			// Stamina Calculator
+		    $(".user-stamina").keyup(function() {
+		    	var stamina = $(this).val();
+		    	minsPerStamina = 3;
 
-		    	if ( ((Math.floor(stamina) == stamina) && ($.isNumeric(stamina))) || (stamina == "") ) {
-					time = stamina * 5;				
+		    	if (((Math.floor(stamina) == stamina) && ($.isNumeric(stamina))) || (stamina == "")) {
+					time = stamina * minsPerStamina;				
 
-					var hours = Math.floor( time / 60);          
+					var hours = Math.floor(time / 60);          
 					var mins = time % 60;			
 
-		    		$( ".in-mins" ).val( hours + " hours and " + mins + " mins"  );
+		    		$(".in-mins").val(hours + " hours and " + mins + " mins" );
 		    	}
 
 		    	else {
 		    		console.log("error");
 		    	}
-
 		    }).keyup();
+
 	    });
     </script>    
 </head>
 
 <body>
 	<div class="container">
-		<h1>My PAD Space</h1><br>
+		<h1>PaD Space</h1><br>
 
 		<h3>Stamina Calculator</h3>
 		<p>Enter number of stamina: <input type="text" class="user-stamina" autofocus></p>
 		<p>Converted to time: <input type="text" class="in-mins" readonly></p>
 
-		<hr>
-
-		<h3>Today's Metal Schedule (PDT)</h3> 
+		<br><hr><br>
 
 		<?php 
-			$url = 'http://puzzledragonx.com/';
-			$content = file_get_contents($url);
-			// echo $content;
-			
-			$temp_content = explode( '<div id="metal1a">' , $content );
-			$metal_sched_table = explode( '<table id="event"' , $temp_content[1] );
-			echo '<table id="event" ' . $metal_sched_table[2];
+			include 'database.php';
 
-			// Break down metal schedule dungeons and times in rows
-			$metal_sched_row = explode( '<tr>' , $metal_sched_table[2]);
-
-			// Grab image number to get dungeon type
-			// var_dump( $metal_sched_row );
-			echo $metal_sched_row[2];
+			date_default_timezone_set('America/New_York');		// Set timezone to New York's
+			$today = date("F j");
 		?>
 
+		<h3>Today's Metal Schedule (PDT)</h3> 
+		<p>Date: <?php echo $today; ?></p>
+		<!-- <?php printMetalSchedule(); ?> -->
 	</div>
 
 </body>
 
 </html>
+
+<?php
+	function printMetalSchedule() {
+		$url = 'http://puzzledragonx.com/';
+		$content = file_get_contents($url);
+		
+		$temp_content = explode('<div id="metal1a">' , $content);
+		$metal_sched_table = explode('<table id="event"' , $temp_content[1]);
+
+		// echo '<table id="event" ' . $metal_sched_table[1];
+
+		echo findDungeonID($metal_sched_table[1]);
+
+		// $dungeonID = strchr($metal_sched_table[1], 'redirect.asp?d=');
+		// $dungeonID = strchr($dungeonID, '" rel="nofollow">', true);
+		// echo $dungeonID;
+
+	}
+
+	function findDungeonID($x) {
+		// $x = strchr($x, 'redirect.asp?d=');
+		// $x = strchr($x, '" rel="nofollow">', true);
+		// $x = explode('=', $x);
+
+		$x = explode('redirect.asp?d=', $x);
+		$x = strchr($x[1], '" rel="nofollow">', true);
+
+		return $x[1];
+	}
+?>
